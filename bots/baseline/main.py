18016cdf15f06d5d8b8cd253ed1747d703b4b890
HerbertTheBird <derekwang643@gmail.com>
2026-03-25 12:00:48 -0700

    baseline bot

---
+++ bots/baseline/main.py
@@ -0,0 +1,41 @@
+# main.py
+
+from cambc import Controller, Direction, EntityType, Position, Environment
+
+import random
+import time
+import sys
+
+import units.builder as builder
+import units.core as core
+import units.turret_gunner as gunner
+import units.turret_sentinel as sentinel
+import units.turret_breach as breach
+
+
+class Player:
+    def __init__(self):
+        self.initialized = False
+    def run(self, c: Controller) -> None:
+        start_time = time.perf_counter()
+        etype = c.get_entity_type()
+
+        if not self.initialized:
+            random.seed(c.get_current_round())
+            if etype == EntityType.CORE:
+                self.me = core
+                self.me.init(c)
+            elif etype == EntityType.BUILDER_BOT:
+                self.me = builder
+                self.me.init(c)  # Ensure builder.init(c) is called
+            else:
+                self.me = turret
+                self.me.init(c)  # Ensure turret.init(c) is called if needed
+            self.initialized = True
+
+        self.me.run()
+
+        end_time = time.perf_counter()
+        print(int((end_time-start_time)*1_000_000),"μs")
+        # if c.get_current_round() < 110:
+        #     print(c.get_id(), c.get_current_round(), (end_time - start_time) * 1000, file=sys.stderr)
