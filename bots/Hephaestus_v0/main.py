9f53f571cca4131c8281fd8577b7ac89b78bcac2
Alex Thummalapalli <alex.thummas66@gmail.com>
2026-04-04 15:39:24 -0700

    fix type

--- bots/Hephaestus_v0/main.py
+++ bots/Hephaestus_v0/main.py
@@ -28,7 +28,7 @@ SPAWN_TURN = -2
 class Player:
     def __init__(self):
         self.initialized = False
-        self.me = None
+        self.me
         
 
     # def _prepare_profile_dir(self, c: Controller) -> None:
